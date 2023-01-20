1f3c2befb38b4c457204d2f49a43b58f1ca64802
Hans Märki <buhtig.hans.maerki@ergoinfo.ch>
2023-01-20 18:25:34 +0000

    Implement Bla

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/bla")
+def bla_world():
+    return render_template("index_bla.html", title="Blabla")
